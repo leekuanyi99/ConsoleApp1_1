69321aa10a2e39a535723a17e494d31d88d27e91
leeku <leeku@USER11>
2025-04-08 18:25:56 +0800

    Commit writeline

--- ConsoleApp1_1/Program.cs
+++ ConsoleApp1_1/Program.cs
@@ -2,8 +2,8 @@
 string str_new = "Welcome! What is your name?";
 Console.WriteLine(str_new);
 string name = Console.ReadLine();
-string str_old = " How long do you want to stay here?";
-Console.WriteLine("Ok, {0}. {1}",name, str_old);
+string str_old = "How long do you want to stay here?";
+Console.WriteLine("Ok,"+name+". "+str_old);
 int number= Convert.ToInt32(Console.ReadLine());
 
 if(number == 0)
